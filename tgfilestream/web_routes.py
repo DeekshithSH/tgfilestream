fc950df91a9ee2e405d8090ec644b4c45295eb3e
Deekshith SH <shdeekshith@gmail.com>
2025-04-05 01:50:21 +0530

    Implement LRU cache to cache FileInfo and prevent fetching same message multipletimes

--- tgfilestream/web_routes.py
+++ tgfilestream/web_routes.py
@@ -13,15 +13,16 @@
 #
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
-from typing import Dict, cast
+from typing import Dict, Optional, cast
 from collections import defaultdict
 import logging
 
 from telethon.tl.custom import Message
 from aiohttp import web
 
+from .cache_util import FileInfo, lru_cache
 from .util import unpack_id, get_file_name, get_requester_ip
-from .config import request_limit
+from .config import request_limit, cache_size
 from .telegram import client, transfer
 
 log = logging.getLogger(__name__)
@@ -56,20 +57,25 @@ def increment_counter(ip: str) -> None:
 def decrement_counter(ip: str) -> None:
     ongoing_requests[ip] -= 1
 
+@lru_cache(cache_size, True)
+async def get_file(file_id: int, expected_name: str) -> Optional[FileInfo]:
+    peer, msg_id = unpack_id(file_id)
+    if not peer or not msg_id:
+        return None
+    message = cast(Message, await client.get_messages(entity=peer, ids=msg_id))
+    if not message or not message.file or get_file_name(message) != expected_name:
+        return None
+    return FileInfo(message)
 
 async def handle_request(req: web.Request, head: bool = False) -> web.Response:
     file_name = req.match_info["name"]
     file_id = int(req.match_info["id"], 16)
-    peer, msg_id = unpack_id(file_id)
-    if not peer or not msg_id:
+
+    file = await get_file(file_id, file_name)
+    if not file:
         return web.Response(status=404, text="404: Not Found")
 
-    message = cast(Message, await client.get_messages(entity=peer, ids=msg_id))
-    print(message)
-    if not message or not message.file or get_file_name(message) != file_name:
-        return web.Response(status=404, text="404: Not Found")
-
-    size = message.file.size
+    size = file.size
     offset = req.http_range.start or 0
     limit = req.http_range.stop or size
 
@@ -77,14 +83,14 @@ async def handle_request(req: web.Request, head: bool = False) -> web.Response:
         ip = get_requester_ip(req)
         if not allow_request(ip):
             return web.Response(status=429)
-        log.info(f"Serving file in {message.id} (chat {message.chat_id}) to {ip}")
-        body = transfer.download(message.media, file_size=size, offset=offset, limit=limit)
+        log.info(f"Serving file in {file.msg_id} (chat {file.chat_id}) to {ip}")
+        body = transfer.download(file.location, file.dc_id, file_size=size, offset=offset, limit=limit)
     else:
         body = None
     return web.Response(status=206 if offset else 200,
                         body=body,
                         headers={
-                            "Content-Type": message.file.mime_type,
+                            "Content-Type": file.mime_type,
                             "Content-Range": f"bytes {offset}-{limit}/{size}",
                             "Content-Length": str(limit - offset),
                             "Content-Disposition": f'attachment; filename="{file_name}"',
